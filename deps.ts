9c1b4f70f4eb4780eb30c9dfc14d3c8a5ed03e89
jia wei <jw397@126.com>
2021-12-15 15:39:51 +0800

    feat: init redis

---
+++ deps.ts
@@ -0,0 +1,24 @@
+export {
+  bgBlue,
+  bgRgb24,
+  bgRgb8,
+  blue,
+  bold,
+  green,
+  italic,
+  red,
+  rgb24,
+  rgb8,
+  yellow,
+} from "https://deno.land/std@0.97.0/fmt/colors.ts";
+
+export { connect } from "https://deno.land/x/redis@v0.24.0/mod.ts";
+export type {
+  Redis,
+  RedisConnectOptions,
+} from "https://deno.land/x/redis@v0.24.0/mod.ts";
+
+export {
+  Inject,
+  Injectable,
+} from "https://deno.land/x/oak_nest@v0.6.5/src/decorators/inject.ts";
